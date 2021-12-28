6e6b78410f8bab4486b68e857b324b8b93a14e00
Joelspitale <vspitale107@alumnos.iua.edu.ar>
2021-12-28 16:22:53 -0300

    tabulaciones

--- src/main/java/ar/edu/iua/iw3/negocio/CargaNegocio.java
+++ src/main/java/ar/edu/iua/iw3/negocio/CargaNegocio.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -144,8 +143,6 @@ public class CargaNegocio implements ICargaNegocio {
             log.error(e.getMessage(), e);
             throw new NegocioException(e);
         }
-
-
     }
 
     @Override
