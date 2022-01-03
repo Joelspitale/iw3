a4bbbfc8a8683bb813d73280ce15c394b3794814
Joelspitale <vspitale107@alumnos.iua.edu.ar>
2022-01-03 07:25:03 -0300

    update

--- src/main/java/ar/edu/iua/iw3/web/Constantes.java
+++ src/main/java/ar/edu/iua/iw3/web/Constantes.java
@@ -2,7 +2,7 @@ package ar.edu.iua.iw3.web;
 
 public final class Constantes {
 
-	public static final String URL_BASE="/api/v1";
+	public static final String URL_BASE="test/api/v1";
 	public static final String URL_AUTH=URL_BASE+"/auth";
 	
 
